b71274c225b10a936f45f1f8a013cd0f3c936500
Semih Güzel <semih_guzel@outlook.com>
2019-02-06 09:55:54 +0300

    Koltuk secimlerinin bir kısmı yapıldı.Tam olarak bitmedi.(projenin patlabilme imkanmına karşı yapıldı)

--- HızlıTrenApp.UI/frmKoltukSecimi.cs
+++ HızlıTrenApp.UI/frmKoltukSecimi.cs
@@ -24,8 +24,6 @@ namespace HızlıTrenApp.UI
             seferId = id;
             saat = tiklananSaat;
         }
-        List<BiletBilgi> businessBiletler;
-        List<BiletBilgi> economyBiletler;
         List<BiletBilgi> businessKadinBiletler;
         List<BiletBilgi> businessErkekBiletler;
         List<BiletBilgi> economyErkekBiletler;
@@ -37,6 +35,7 @@ namespace HızlıTrenApp.UI
         string businessKadinKoltuk = @"..\..\Images\Resized_Seats\seat_woman_resized_business.png";
         string businessBosKoltuk = @"..\..\Images\Resized_Seats\seat_available_resized_business.png";
         string economyBosKoltuk = @"..\..\Images\Resized_Seats\seat_available_resized_economy.png";
+        string economyLuggage = @"..\..\Images\luggage_economy.png";
         private void frmKoltukSecimi_Load(object sender, EventArgs e)
         {
             SeferSaatleriDal ssd = new SeferSaatleriDal();
@@ -157,14 +156,14 @@ namespace HızlıTrenApp.UI
 
             int businessSoldanBosluk = 60;
             int businessKoltukArasi = 20;
-            int businessUsttenBosluk = 20;
+            int businessUsttenBosluk = 10;
             int businessKoltukSayisi = 8;
 
             lblB1Masa = new Label();
             lblB1Masa.AutoSize = false;
             lblB1Masa.TextAlign = ContentAlignment.MiddleCenter;
             lblB1Masa.Width = 269;
-            lblB1Masa.Height = 40;
+            lblB1Masa.Height = 20;
             lblB1Masa.Text = "MASA";
             lblB1Masa.BackColor = Color.LightGoldenrodYellow;
             grpBusiness1.Controls.Add(lblB1Masa);
@@ -173,7 +172,7 @@ namespace HızlıTrenApp.UI
             lblB2Masa.AutoSize = false;
             lblB2Masa.TextAlign = ContentAlignment.MiddleCenter;
             lblB2Masa.Width = 269;
-            lblB2Masa.Height = 40;
+            lblB2Masa.Height = 20;
             lblB2Masa.Text = "MASA";
             lblB2Masa.BackColor = Color.LightGoldenrodYellow;
             grpBusiness2.Controls.Add(lblB2Masa);
@@ -185,7 +184,7 @@ namespace HızlıTrenApp.UI
                 lblB1 = new Label();
                 pbB1.Name = "B" + i;
                 lblB1.Text = pbB1.Name;
-                BusinessKoltuk(pbB1, lblB1);
+                KoltukAyarlari(pbB1, lblB1, null);
 
 
 
@@ -199,7 +198,7 @@ namespace HızlıTrenApp.UI
                 pbB2.Name = "B" + businessKoltukSayisi;
                 lblB2.Text = pbB2.Name;
                 businessKoltukSayisi++;
-                BusinessKoltuk(pbB2, lblB2);
+                KoltukAyarlari(pbB2, lblB2, null);
 
 
                 grpBusiness2.Controls.Add(pbB2);
@@ -220,25 +219,28 @@ namespace HızlıTrenApp.UI
                 {
                     //grpBusiness1'de alt 4 koltuk
                     pbB1.Location = new Point(((i - 4) * businessSoldanBosluk) + businessKoltukArasi, lblB1.Height + pbB1.Height + (businessUsttenBosluk * 4));
-                    lblB1.Location = new Point(pbB1.Location.X, pbB1.Location.Y + (businessUsttenBosluk * 2));
+                    lblB1.Location = new Point(pbB1.Location.X, pbB1.Location.Y + (businessUsttenBosluk * 3));
 
                     //grpBusiness2'de alt 4 koltuk
                     pbB2.Location = new Point(((i - 4) * businessSoldanBosluk) + businessKoltukArasi, lblB2.Height + pbB2.Height + (businessUsttenBosluk * 4));
-                    lblB2.Location = new Point(pbB2.Location.X, pbB2.Location.Y + (businessUsttenBosluk * 2));
+                    lblB2.Location = new Point(pbB2.Location.X, pbB2.Location.Y + (businessUsttenBosluk * 3));
                 }
-                lblB1Masa.Location = new Point(0, 80);
-                lblB2Masa.Location = new Point(0, 80);
+                lblB1Masa.Location = new Point(0, 60);
+                lblB2Masa.Location = new Point(0, 60);
             }
             //Economy pb ve lbl
             PictureBox pbE1;
             PictureBox pbE2;
             Label lblE1;
             Label lblE2;
+            PictureBox luggage;
+            PictureBox luggage2;
+
             //Economy Degiskenleri
 
             int economySoldanBosluk = 60;
             int economyKoltukArasi = 20;
-            int economyUsttenBosluk = 20;
+            int economyUsttenBosluk = 10;
             int economyLabelBosluk = 15;
             int economyKoltukSayisi = 12;
             for (int i = 0; i < 12; i++)
@@ -246,75 +248,89 @@ namespace HızlıTrenApp.UI
                 //grpEconomy1
                 pbE1 = new PictureBox();
                 lblE1 = new Label();
+                luggage = new PictureBox();
                 pbE1.Name = "E" + i;
                 lblE1.Text = pbE1.Name;
-                EconomyKoltuk(pbE1, lblE1);
+                KoltukAyarlari(pbE1, lblE1, luggage);
                 grpEconomy1.Controls.Add(pbE1);
                 grpEconomy1.Controls.Add(lblE1);
+                grpEconomy1.Controls.Add(luggage);
 
                 //grpEconomy2
                 pbE2 = new PictureBox();
                 lblE2 = new Label();
+                luggage2 = new PictureBox();
                 pbE2.Name = "E" + economyKoltukSayisi;
                 lblE2.Text = pbE2.Name;
                 economyKoltukSayisi++;
-                EconomyKoltuk(pbE2, lblE2);
+                KoltukAyarlari(pbE2, lblE2, luggage2);
                 grpEconomy2.Controls.Add(pbE2);
                 grpEconomy2.Controls.Add(lblE2);
 
-                if (i < 4)
+                if (i < 3)
                 {
                     //grpEconomy1'de ust 4 koltuk
-                    pbE1.Location = new Point((i * economySoldanBosluk) + economyKoltukArasi, economyUsttenBosluk);
-                    lblE1.Location = new Point(pbE1.Location.X, pbE1.Height + economyUsttenBosluk + 2);
+                    pbE1.Location = new Point((2 * i * economySoldanBosluk) + economyKoltukArasi, economyUsttenBosluk);
+                    lblE1.Location = new Point(pbE1.Location.X, pbE1.Height + economyUsttenBosluk - 3);
+                    luggage.Location = new Point(pbE1.Location.X + economySoldanBosluk - 5, economyUsttenBosluk);
 
                     //grpEconomy2'de ust 4 koltuk
                     pbE2.Location = new Point((i * economySoldanBosluk) + economyKoltukArasi, economyUsttenBosluk);
-                    lblE2.Location = new Point(pbE2.Location.X, pbE2.Height + economyUsttenBosluk + 2);
+                    lblE2.Location = new Point(pbE2.Location.X, pbE2.Height + economyUsttenBosluk - 3);
                 }
-                else if (i >= 4 && i < 8)
+                else if (i >= 3 && i < 10)
                 {
                     //grpEconomy1'de orta 4 koltuk
-                    pbE1.Location = new Point(((i - 4) * businessSoldanBosluk) + businessKoltukArasi, lblE1.Height + pbE1.Height + economyUsttenBosluk + 5);
-                    lblE1.Location = new Point(pbE1.Location.X, pbE1.Location.Y + economyUsttenBosluk + economyLabelBosluk);
+                    pbE1.Location = new Point(((i - 4) * businessSoldanBosluk) + businessKoltukArasi, lblE1.Height + pbE1.Height + economyUsttenBosluk);
+                    lblE1.Location = new Point(pbE1.Location.X, pbE1.Location.Y + economyUsttenBosluk + economyLabelBosluk + 3);
 
                     //grpEconomy2'de orta 4 koltuk
-                    pbE2.Location = new Point(((i - 4) * businessSoldanBosluk) + businessKoltukArasi, lblE2.Height + pbE2.Height + economyUsttenBosluk + 5);
-                    lblE2.Location = new Point(pbE2.Location.X, pbE2.Location.Y + economyUsttenBosluk + economyLabelBosluk);
+                    pbE2.Location = new Point(((i - 4) * businessSoldanBosluk) + businessKoltukArasi, lblE2.Height + pbE2.Height + economyUsttenBosluk);
+                    lblE2.Location = new Point(pbE2.Location.X, pbE2.Location.Y + economyUsttenBosluk + economyLabelBosluk + 3);
                 }
                 else
                 {
                     //grpEconomy1'de alt 4 koltuk
-                    pbE1.Location = new Point(((i - 8) * businessSoldanBosluk) + businessKoltukArasi, lblE1.Height + pbE1.Height + economyUsttenBosluk + (economyUsttenBosluk * 3) + 5);
-                    lblE1.Location = new Point(pbE1.Location.X, pbE1.Location.Y + economyUsttenBosluk + 15);
+                    grpEconomy1.Controls.Add(pbE1);
+                    grpEconomy1.Controls.Add(lblE1);
+                    grpEconomy1.Controls.Add(luggage);
+                    luggage.Location = new Point(((i - 10) * businessSoldanBosluk) + businessKoltukArasi, lblE1.Height + pbE1.Height + economyUsttenBosluk + (economyUsttenBosluk * 3) + economyUsttenBosluk);
+                    pbE1.Location = new Point((2 * (i - 10) * businessSoldanBosluk) + businessKoltukArasi, lblE1.Height + pbE1.Height + economyUsttenBosluk + (economyUsttenBosluk * 3) + economyUsttenBosluk);
+                        lblE1.Location = new Point(pbE1.Location.X, pbE1.Location.Y + economyUsttenBosluk + 20);
+                        
 
                     //grpEconomy2'de alt 4 koltuk
-                    pbE2.Location = new Point(((i - 8) * businessSoldanBosluk) + businessKoltukArasi, lblE2.Height + pbE2.Height + economyUsttenBosluk + (economyUsttenBosluk * 3) + 5);
-                    lblE2.Location = new Point(pbE2.Location.X, pbE2.Location.Y + economyUsttenBosluk + 15);
+                    pbE2.Location = new Point(((i - 10) * businessSoldanBosluk) + businessKoltukArasi - 5, lblE2.Height + pbE2.Height + economyUsttenBosluk + (economyUsttenBosluk * 3) + economyUsttenBosluk + 5);
+                    lblE2.Location = new Point(pbE2.Location.X, pbE2.Location.Y + economyUsttenBosluk + 20);
                 }
             }
         }
 
-        private void EconomyKoltuk(PictureBox pb, Label lbl)
+        private void KoltukAyarlari(PictureBox pb, Label lbl, PictureBox luggage)
         {
             pb.Width = 40;
             pb.Height = 30;
             lbl.TextAlign = ContentAlignment.MiddleCenter;
             lbl.Width = pb.Width;
-            lbl.Height = 20;
-
+            lbl.Height = 15;
             pb.ImageLocation = economyBosKoltuk;
+            if (luggage != null)
+            {
+                luggage.Width = 40;
+                luggage.Height = 30;
+                luggage.ImageLocation = economyLuggage;
+            }
         }
 
         private void BusinessKoltuk(PictureBox pb, Label lbl)
         {
-            pb.Width = 50;
-            pb.Height = 40;
+            pb.Width = 40;
+            pb.Height = 30;
             lbl.TextAlign = ContentAlignment.MiddleCenter;
             lbl.Width = pb.Width;
             lbl.Height = 15;
             pb.Click += Pb_Click;
-            pb.ImageLocation = businessBosKoltuk;
+            pb.ImageLocation = economyBosKoltuk;
 
         }
 
